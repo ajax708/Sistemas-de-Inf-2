3c6c3c279e5f18ed4270290e72a4d95d94a86483
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2018-12-10 12:29:08 -0400

    detalles de home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,48 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Bienvenido a SysLab</h1>
 @stop
 
 @section('content')
-    <p>You are logged in!</p>
+<!-- Small boxes (Stat box) -->
+@php
+$pendientes = App\Orden::where('estado','En Proceso')->count();
+$completo = App\Orden::where('estado','Completo')->count();
+@endphp
+<div class="row">
+  <div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-aqua">
+      <div class="inner">
+         
+        <h3>{{$pendientes}}</h3>
+
+        <p>Ordenes pendientes</p>
+      </div>
+      <div class="icon">
+        <i class="fas fa-clock"></i>
+      </div>
+      <a href="#" class="small-box-footer">Ver ordenes <i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  <!-- ./col -->
+  <div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-green">
+      <div class="inner">
+        <h3>{{$completo}}</h3>
+
+        <p>Ordenes Completas a entregar</p>
+      </div>
+      <div class="icon">
+        <i class="fas fa-check-double"></i>
+      </div>
+      <a href="#" class="small-box-footer">Ver<i class="fa fa-arrow-circle-right"></i></a>
+    </div>
+  </div>
+  
+  <!-- ./col -->
+</div>
+
 @stop
